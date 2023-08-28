e4aa220f71299818bb64a17a2409d518666a9ec0
AVolkodav <143172261+AVolkodav@users.noreply.github.com>
2023-08-28 17:34:10 +0300

    feat сделал вторую часть задания

--- script.ts
+++ script.ts
@@ -1,56 +1,67 @@
-import express from "express";
-import { Request, Response } from "express";
-import config  from "./config";
-import  AmoCRM  from "./api/amo";
+import express from 'express'
+import { type Request, type Response } from 'express'
+import config from './config'
+import AmoCRM from './api/amo'
 
-const router = express.Router();
-const SERVICE_FIELD_ID = 1212207;
-const api = new AmoCRM(config.SUB_DOMAIN, config.AUTH_CODE);
+const router = express.Router()
+const SERVICE_FIELD_ID = 1212207
+const CHECK_TASK_TYPE_ID = 3044666
+const TASK_CREATE_DATA = {
+  task_type_id: CHECK_TASK_TYPE_ID,
+  text: 'Проверить бюджет',
+  complete_till: Math.round(Date.now() / 1000 + 60 * 60 * 24),
+  entity_type: 'leads'
+}
+const api = new AmoCRM(config.SUB_DOMAIN, config.AUTH_CODE)
 
-function getTotalSum(contactFields: any, checkedSevices: any): number {
-  let totalSum = 0;
-  for (let service of checkedSevices) {
-    for (let field of contactFields){
-      if (field.field_name == service.value) {
-        totalSum += +field.values[0].value;
-        break;
+function getTotalSum (contactFields: any, checkedSevices: any): number {
+  let totalSum = 0
+  for (const service of checkedSevices) {
+    for (const field of contactFields) {
+      if (field.field_name === service.value) {
+        totalSum += +field.values[0].value
+        break
       }
     }
   }
-  return totalSum;
+  return totalSum
 }
 
-function getMainContactId(links: any) {
-  for( let link of links) {
-    if (link.metadata.main_contact) {
-      return link.to_entity_id;
+function getMainContactId (links: any): number {
+  for (const link of links) {
+    if (link.metadata.main_contact === true) {
+      return link.to_entity_id
     }
   }
-  return -1;
+  return -1
 }
 
+// eslint-disable-next-line @typescript-eslint/no-misused-promises
 router.post('/', async (req: Request, res: Response) => {
-	const firstWebHook = req.body.leads.update[0].modified_user_id != 0;
-  const updatedFieldId = req.body.leads.update[0].custom_fields[0].id;
-  if (updatedFieldId == SERVICE_FIELD_ID && firstWebHook) {
-		  const leadId = req.body.leads.update[0].id;
-      const lead = await api.getLeadLinks(leadId);
-      const contactId = getMainContactId(lead._embedded.links);
-      if (contactId == -1){
-        return;
-      }
-      const contact = await api.getContact(contactId);
-      if (contact) {
-        const checkedServices = req.body.leads.update[0].custom_fields[0].values
-        const price = getTotalSum(contact.custom_fields_values, checkedServices);
-        const leadUpdateData = {
-          id: leadId,
-          price: price
-        }
-        await api.updateDeals(leadId, leadUpdateData);
-      }
-      console.log('updated');
+  const lead = req.body.leads.update[0]
+  const firstWebHook = +lead.modified_user_id !== 0
+  const updatedFieldId = lead.custom_fields[0].id
+  if (!(+updatedFieldId === SERVICE_FIELD_ID && firstWebHook)) {
+    return
   }
+  const leadLinks = await api.getLeadLinks(lead.id)
+  const contactId = getMainContactId(leadLinks._embedded.links)
+  if (contactId === -1) {
+    return
+  }
+  const contact = await api.getContact(contactId)
+  if (contact !== undefined) {
+    const checkedServices = lead.custom_fields[0].values
+    const price = getTotalSum(contact.custom_fields_values, checkedServices)
+    const leadUpdateData = {
+      id: lead.id,
+      price
+    }
+    await api.updateDeals(lead.id, leadUpdateData)
+  }
+  console.log('updated')
+  const responsibleUserId = +lead.responsible_user_id
+  await api.createTask([{ ...TASK_CREATE_DATA, entity_id: +lead.id, responsible_user_id: responsibleUserId }])
 })
 
-export default router;
+export default router
