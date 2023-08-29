7ee832ca2fa2925b24cf99374e71faa85d175cff
AVolkodav <143172261+AVolkodav@users.noreply.github.com>
2023-08-29 13:14:11 +0300

    удалил ненужные файлы

--- dist/types/fields/fieldList.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Response for :
-// GET /api/v4/leads/custom_fields
-// GET /api/v4/contacts/custom_fields
-// GET /api/v4/companies/custom_fields
-// GET /api/v4/customers/custom_fields
-// GET /api/v4/customers/segments/custom_fields
-// GET /api/v4/catalogs/{catalog_id}/custom_fields
-Object.defineProperty(exports, "__esModule", { value: true });
--- dist/types/pipeline/pipelineList.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-// response for request GET /api/v4/leads/pipelines
-Object.defineProperty(exports, "__esModule", { value: true });
